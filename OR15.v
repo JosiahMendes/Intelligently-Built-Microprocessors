a6babf0531c7e36f27b6e01e67bd9466eb8c8011
EdvardJSHolen <ejh19@ic.ac.uk>
2020-05-29 10:13:51 +0100

    Finished a wallace multiplier with  a carry bit. Will now start working on the entire ALU.

---
+++ OR15.v
@@ -0,0 +1,6 @@
+
+module OR15(input A,B,C,D,E,F,G,H,I,J,K,L,M,N,O, output Cout);
+
+assign Cout = A|B|C|D|E|F|G|H|I|J|K|L|M|N|O; 
+
+endmodule
---
+++ SecondFullAdder.v
@@ -0,0 +1,7 @@
+module SecondFullAdder(input A, input B, input C_in, output C_out, output S);
+
+assign C_out = (A & B)|(B & C_in)|(A & C_in);
+
+assign S = A ^ B ^ C_in;
+
+endmodule
