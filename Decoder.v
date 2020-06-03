d7fa2e133459a87f7ad0da41f032925eee77b12c
Josiah Mendes <jam419@ic.ac.uk>
2020-06-03 01:56:37 +0100

    LDI and STP working

--- Decoder.v
+++ Decoder.v
@@ -1,27 +1,86 @@
-module Decoder ()
+module Decoder (
+	input [15:0] INSTR,
+	output reg [15:0] q,
+	
+	input f, e1, e2, e3,
+	
+	output  instr_wren, instr_rden, data_wren, data_rden, pc_sload, pc_cnten, r0en, r1en, r2en, r3en, mux1_sel, extra1, extra2
+	
+	);
 
-input [4..0] ir;
+
+
+wire A, B,C, D, E, F, G, H, I, J, K, L, M, N, O, P;
+
+assign A = INSTR[15];
+assign B = INSTR[14];
+assign C = INSTR[13];
+assign D = INSTR[12];
+assign E = INSTR[11];
+assign F = INSTR[10];
+assign G = INSTR[9];
+assign H = INSTR[8];
+assign I = INSTR[7];
+assign J = INSTR[6];
+assign K = INSTR[5];
+assign L = INSTR[4];
+assign M = INSTR[3];
+assign N = INSTR[2];
+assign O = INSTR[1];
+assign P = INSTR[0];
+
+
+wire stp, adr, adm, adi, sbr, sbm, sbi, mlr, mlm, xsl, xsr, bbo, bfe, jmr, jmp, ldi, sta, ldr, sti, psh, pop, lda;
+
+assign stp = ~A & ~B & ~C & ~D & ~E;
+assign adr = ~A & ~B & ~C & ~D &  E; 
+assign adm = ~A & ~B & ~C &  D & ~E;
+assign adi = ~A & ~B & ~C &  D &  E;
+assign sbr = ~A & ~B &  C & ~D & ~E;
+assign sbm = ~A & ~B &  C & ~D &  E;
+assign sbi = ~A & ~B &  C &  D & ~E;    
+assign mlr = ~A & ~B &  C &  D &  E;
+assign mlm = ~A &  B & ~C & ~D & ~E; 
+assign xsl = ~A &  B & ~C & ~D &  E; 
+assign xsr = ~A &  B & ~C &  D & ~E;
+assign bbo = ~A &  B & ~C &  D &  E;
+assign bfe = ~A &  B &  C & ~D;
+assign jmr = ~A &  B &  C &  D & ~E;
+assign jmp = ~A &  B &  C &  D &  E;
+assign ldi =  A & ~B & ~C;
+assign sta =  A & ~B &  C;
+assign ldr =  A &  B & ~C & ~D & ~E;
+assign sti =  A &  B & ~C & ~D &  E;
+assign psh =  A &  B & ~C &  D & ~E;
+assign pop =  A &  B & ~C &  D &  E;
+assign lda =  A &  B &  C;
+
+assign pc_cnten = e1 & (adr | adm | adi | sbr| sbm | sbi | mlr | mlm | xsl | xsr | bbo | bfe | ldi| sta | ldr | sti | psh | pop |lda);
+assign pc_sload = e1 & (jmp);
+
+assign instr_wren = 0;
+assign instr_rden = f;
+
+assign data_wren = 0;
+assign data_rden = 1;
+
+assign r0en = (ldi & ~D & ~E & e1);
+assign r1en = (ldi & ~D &  E & e1);
+assign r2en = (ldi &  D & ~E & e1);
+assign r3en = (ldi &  D &  E & e1);
+
+assign mux1_sel = (ldi&e1);
 
 
 
-assign stp = !ir[4] & !ir[3] & !ir[2] & !ir[1] & !ir[0];
-assign adr = !ir[4] & !ir[3] & !ir[2] & !ir[1] &  ir[0];
-assign adm = !ir[4] & !ir[3] & !ir[2] &  ir[1] & !ir[0];
-assign adi = !ir[4] & !ir[3] & !ir[2] &  ir[1] &  ir[0];
-assign sbr = !ir[4] & !ir[3] &  ir[2] & !ir[1] & !ir[0];
-assign sbm = !ir[4] & !ir[3] &  ir[2] & !ir[1] &  ir[0];
-assign sbi = !ir[4] & !ir[3] &  ir[2] &  ir[1] & !ir[0];
-assign mlr = !ir[4] & !ir[3] &  ir[2] &  ir[1] &  ir[0];
-assign mlm = !ir[4] &  ir[3] & !ir[2] & !ir[1] & !ir[0];
-assign xsl = !ir[4] &  ir[3] & !ir[2] & !ir[1] &  ir[0];
-assign xsr = !ir[4] &  ir[3] & !ir[2] &  ir[1] & !ir[0];
-assign bbo = !ir[4] &  ir[3] & !ir[2] &  ir[1] &  ir[0];
-assign bfe = !ir[4] &  ir[3] &  ir[2] & !ir[1];
-assign jmr = !ir[4] &  ir[3] &  ir[2] &  ir[1] &  ir[0];
-assign jmp = !ir[4] &  ir[3] &  ir[2] &  ir[1] &
-assign ldi =  ir[4] & !ir[3] & !ir[2];
-assign sta =  ir[4] & !ir[3] &  ir[2];
-assign ldr =  ir[4] &  ir[3] & !ir[2] & !ir[1] & !ir[0];
-assign sti =  ir[4] &  ir[3] & !ir[2] & !ir[1] &  ir[0];
-assign psh =  ir[4] &  ir[3] & !ir[2] &  ir[1] & !ir[0];
-assign pop =  ir[4] &  ir[3] & !ir[2] &  ir[1] &  ir[0];
+always @(*)
+	if (ldi&e1) begin 
+		 q[15:11] =  5'b0;
+		 q[10:0] = INSTR[10:0];
+	end
+		
+
+
+
+
+endmodule
--- RAMTest.v
+++ RAMTest.v
@@ -4,7 +4,7 @@
 // MODULE: altsyncram 
 
 // ============================================================
-// File Name: RAM.v
+// File Name: RAMTest.v
 // Megafunction Name(s):
 // 			altsyncram
 //
@@ -37,7 +37,7 @@
 // synopsys translate_off
 `timescale 1 ps / 1 ps
 // synopsys translate_on
-module RAM (
+module RAMTest (
 	address,
 	clock,
 	data,
@@ -45,7 +45,7 @@ module RAM (
 	wren,
 	q);
 
-	input	[10:0]  address;
+	input	[7:0]  address;
 	input	  clock;
 	input	[15:0]  data;
 	input	  rden;
@@ -94,13 +94,13 @@ module RAM (
 		altsyncram_component.intended_device_family = "Cyclone V",
 		altsyncram_component.lpm_hint = "ENABLE_RUNTIME_MOD=NO",
 		altsyncram_component.lpm_type = "altsyncram",
-		altsyncram_component.numwords_a = 2048,
+		altsyncram_component.numwords_a = 256,
 		altsyncram_component.operation_mode = "SINGLE_PORT",
 		altsyncram_component.outdata_aclr_a = "NONE",
 		altsyncram_component.outdata_reg_a = "UNREGISTERED",
 		altsyncram_component.power_up_uninitialized = "FALSE",
 		altsyncram_component.read_during_write_mode_port_a = "NEW_DATA_NO_NBE_READ",
-		altsyncram_component.widthad_a = 11,
+		altsyncram_component.widthad_a = 8,
 		altsyncram_component.width_a = 16,
 		altsyncram_component.width_byteena_a = 1;
 
@@ -130,7 +130,7 @@ endmodule
 // Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 // Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
 // Retrieval info: PRIVATE: MIFfilename STRING "Test.mif"
-// Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "2048"
+// Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "256"
 // Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 // Retrieval info: PRIVATE: READ_DURING_WRITE_MODE_PORT_A NUMERIC "3"
 // Retrieval info: PRIVATE: RegAddr NUMERIC "1"
@@ -140,7 +140,7 @@ endmodule
 // Retrieval info: PRIVATE: SingleClock NUMERIC "1"
 // Retrieval info: PRIVATE: UseDQRAM NUMERIC "1"
 // Retrieval info: PRIVATE: WRCONTROL_ACLR_A NUMERIC "0"
-// Retrieval info: PRIVATE: WidthAddr NUMERIC "11"
+// Retrieval info: PRIVATE: WidthAddr NUMERIC "8"
 // Retrieval info: PRIVATE: WidthData NUMERIC "16"
 // Retrieval info: PRIVATE: rden NUMERIC "1"
 // Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
@@ -150,31 +150,31 @@ endmodule
 // Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone V"
 // Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
-// Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "2048"
+// Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "256"
 // Retrieval info: CONSTANT: OPERATION_MODE STRING "SINGLE_PORT"
 // Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: OUTDATA_REG_A STRING "UNREGISTERED"
 // Retrieval info: CONSTANT: POWER_UP_UNINITIALIZED STRING "FALSE"
 // Retrieval info: CONSTANT: READ_DURING_WRITE_MODE_PORT_A STRING "NEW_DATA_NO_NBE_READ"
-// Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "11"
+// Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "8"
 // Retrieval info: CONSTANT: WIDTH_A NUMERIC "16"
 // Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
-// Retrieval info: USED_PORT: address 0 0 11 0 INPUT NODEFVAL "address[10..0]"
+// Retrieval info: USED_PORT: address 0 0 8 0 INPUT NODEFVAL "address[7..0]"
 // Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
 // Retrieval info: USED_PORT: data 0 0 16 0 INPUT NODEFVAL "data[15..0]"
 // Retrieval info: USED_PORT: q 0 0 16 0 OUTPUT NODEFVAL "q[15..0]"
 // Retrieval info: USED_PORT: rden 0 0 0 0 INPUT VCC "rden"
 // Retrieval info: USED_PORT: wren 0 0 0 0 INPUT NODEFVAL "wren"
-// Retrieval info: CONNECT: @address_a 0 0 11 0 address 0 0 11 0
+// Retrieval info: CONNECT: @address_a 0 0 8 0 address 0 0 8 0
 // Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
 // Retrieval info: CONNECT: @data_a 0 0 16 0 data 0 0 16 0
 // Retrieval info: CONNECT: @rden_a 0 0 0 0 rden 0 0 0 0
 // Retrieval info: CONNECT: @wren_a 0 0 0 0 wren 0 0 0 0
 // Retrieval info: CONNECT: q 0 0 16 0 @q_a 0 0 16 0
-// Retrieval info: GEN_FILE: TYPE_NORMAL RAM.v TRUE
-// Retrieval info: GEN_FILE: TYPE_NORMAL RAM.inc FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL RAM.cmp FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL RAM.bsf TRUE
-// Retrieval info: GEN_FILE: TYPE_NORMAL RAM_inst.v FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL RAM_bb.v TRUE
+// Retrieval info: GEN_FILE: TYPE_NORMAL RAMTest.v TRUE
+// Retrieval info: GEN_FILE: TYPE_NORMAL RAMTest.inc FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL RAMTest.cmp FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL RAMTest.bsf TRUE
+// Retrieval info: GEN_FILE: TYPE_NORMAL RAMTest_inst.v FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL RAMTest_bb.v TRUE
 // Retrieval info: LIB_FILE: altera_mf
