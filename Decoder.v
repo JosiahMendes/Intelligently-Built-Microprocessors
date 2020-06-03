3a5d0d93300d71e42ec6b8d6ab0ac41beb9f5bdd
Josiah Mendes <jam419@ic.ac.uk>
2020-06-03 18:55:29 +0100

    LDR without offsets, STA, LDA Implemented

--- Decoder.v
+++ Decoder.v
@@ -1,10 +1,13 @@
 module Decoder (
 	input [15:0] INSTR,
 	output reg [15:0] q,
+	output reg [1:0]out_sel,
 	
-	input f, e1, e2, e3,
+	input fe, e1, e2, 
 	
-	output  instr_wren, instr_rden, data_wren, data_rden, pc_sload, pc_cnten, r0en, r1en, r2en, r3en, mux1_sel, extra1, extra2
+	output  instr_wren, instr_rden, data_wren, data_rden, pc_sload, pc_cnten, r0en, r1en, r2en, r3en, extra1, mux1_sel, mux2_sel 
+	
+	
 	
 	);
 
@@ -30,9 +33,10 @@ assign O = INSTR[1];
 assign P = INSTR[0];
 
 
-wire stp, adr, adm, adi, sbr, sbm, sbi, mlr, mlm, xsl, xsr, bbo, bfe, jmr, jmp, ldi, sta, ldr, sti, psh, pop, lda;
+wire stp, adr, adm, adi, sbr, sbm, sbi, mlr, bfe, xsl, xsr, bbo, stk, ldr, sti, ldi, sta, lda, jmr, jmp, jeq, jnq;
 
 assign stp = ~A & ~B & ~C & ~D & ~E;
+
 assign adr = ~A & ~B & ~C & ~D &  E; 
 assign adm = ~A & ~B & ~C &  D & ~E;
 assign adi = ~A & ~B & ~C &  D &  E;
@@ -40,46 +44,57 @@ assign sbr = ~A & ~B &  C & ~D & ~E;
 assign sbm = ~A & ~B &  C & ~D &  E;
 assign sbi = ~A & ~B &  C &  D & ~E;    
 assign mlr = ~A & ~B &  C &  D &  E;
-assign mlm = ~A &  B & ~C & ~D & ~E; 
-assign xsl = ~A &  B & ~C & ~D &  E; 
-assign xsr = ~A &  B & ~C &  D & ~E;
-assign bbo = ~A &  B & ~C &  D &  E;
-assign bfe = ~A &  B &  C & ~D;
-assign jmr = ~A &  B &  C &  D & ~E;
-assign jmp = ~A &  B &  C &  D &  E;
+assign bfe = ~A &  B & ~C & ~D; 
+assign xsl = ~A &  B & ~C &  D & ~E; 
+assign xsr = ~A &  B & ~C &  D &  E;
+assign bbo = ~A &  B &  C & ~D & ~E;
+
+assign stk = ~A &  B &  C & ~D &  E;
+assign ldr = ~A &  B &  C &  D & ~E;
+assign sti =  A &  B &  C &  D &  E;
 assign ldi =  A & ~B & ~C;
 assign sta =  A & ~B &  C;
-assign ldr =  A &  B & ~C & ~D & ~E;
-assign sti =  A &  B & ~C & ~D &  E;
-assign psh =  A &  B & ~C &  D & ~E;
-assign pop =  A &  B & ~C &  D &  E;
-assign lda =  A &  B &  C;
+assign lda =  A &  B & ~C;
 
-assign pc_cnten = e1 & (adr | adm | adi | sbr| sbm | sbi | mlr | mlm | xsl | xsr | bbo | bfe | ldi| sta | ldr | sti | psh | pop |lda);
+assign jmr =  A &  B &  C & ~D & ~E;
+assign jmp =  A &  B &  C & ~D &  E;
+assign jeq =  A &  B &  C &  D & ~E;
+assign jnq =  A &  B &  C &  D &  E;
+
+
+assign pc_cnten = e1 & (adr | adm | adi | sbr| sbm | sbi | mlr | xsl | xsr | bbo | bfe | ldi| sta | ldr | sti | stk| lda);
 assign pc_sload = e1 & (jmp);
 
 assign instr_wren = 0;
-assign instr_rden = f;
+assign instr_rden = fe;
 
-assign data_wren = 0;
+assign data_wren = (sta&e1);
 assign data_rden = 1;
 
-assign r0en = (ldi & ~D & ~E & e1);
-assign r1en = (ldi & ~D &  E & e1);
-assign r2en = (ldi &  D & ~E & e1);
-assign r3en = (ldi &  D &  E & e1);
+assign r0en = (ldi & ~D & ~E & e1) | (lda & ~D & ~E & e2) | (ldr & ~F & ~G & e2);
+assign r1en = (ldi & ~D &  E & e1) | (lda & ~D &  E & e2) | (ldr & ~F &  G & e2);
+assign r2en = (ldi &  D & ~E & e1) | (lda &  D & ~E & e2) | (ldr &  F & ~G & e2);
+assign r3en = (ldi &  D &  E & e1) | (lda &  D &  E & e2) | (ldr &  F &  G & e2);
 
 assign mux1_sel = (ldi&e1);
-
+assign mux2_sel = (ldr&e1);
+assign extra1 = (lda)|(ldr);
 
 
 always @(*)
-	if (ldi&e1) begin 
+	if ((ldi&e1)|(lda&e1)|(sta&e1)) begin 
 		 q[15:11] =  5'b0;
 		 q[10:0] = INSTR[10:0];
-	end
-		
-
+	end else 
+		q[15:0] = INSTR[10:0];
+	
+always @(*)
+	if (sta&e1)
+		out_sel[1:0] = INSTR[12:11];
+	else if (ldr&e1)
+		out_sel[1:0] = INSTR[7:6];
+	else 
+		out_sel[1:0] = 2'b0;
 
 
 
