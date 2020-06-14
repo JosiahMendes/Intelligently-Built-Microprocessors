187d8c9876c0954f8c4368d0b97aec59c49e5e5c
jam419 <jam419@imperial.ac.uk>
2020-06-14 01:27:39 +0100

    CodeReferencingforStack

--- LIFOBlock.v
+++ LIFOBlock.v
@@ -1,3 +1,6 @@
+//Adapted from https://www.beyond-circuits.com/wordpress/2009/10/lifo/ with permission from author, Pete Johnson
+//Adapted from https://esrd2014.blogspot.com/p/last-in-first-out-buffer.html
+
 module LIFOBlock #(parameter depth = 16, log2_depth = 4, log2_depthp1 = 5)
 (
 	input[15:0] data,
@@ -10,15 +13,14 @@ module LIFOBlock #(parameter depth = 16, log2_depth = 4, log2_depthp1 = 5)
 
 
 reg [log2_depthp1-1:0] count;
-
-wire writing = push && ( count < depth || pop);
-wire reading = pop && count > 0;
-
 reg[log2_depthp1-1:0] next_count;
+
+wire writing = push && (pop || count < depth );
+wire reading = pop && count > 0;
 
 always @(*)
 	if (writing && !reading)
-		next_count = count+1;
+		next_count = count+1; //Combinational logic to determine next state of stack. 
 	else if (reading && !writing)
 		next_count = count-1;
 	else
@@ -28,15 +30,15 @@ always @(posedge clock)
 	count <= next_count;
 
 always @(posedge clock)
-	full <= next_count == depth;
+	full <= next_count == depth; //Checks if stack is Full.
 	
 always @(posedge clock)
-	empty <= next_count == 0;
+	empty <= next_count == 0; //Checks if stack is empty.
 	
 
 wire[5:0] stackPointer = writing ? count[log2_depth-1:0] : (count[log2_depth-1:0])-1;
 
-reg [15:0] stack[depth-1:0];
+reg [15:0] stack[depth-1:0]; 
 
 always @(posedge clock)
 	if (writing && !reading)
